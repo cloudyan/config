be63bba81b89e5a4f287d6e4bbdcdead51e71177
cloudyan <1395093509@qq.com>
2020-07-29 23:30:45 +0800

    feat: update

--- test/es6.js
+++ test/es6.js
@@ -1,8 +1,11 @@
 
-// import { add } from './es'
+import { add } from './es'
 
-// 还不支持 es6
-// console.log(add(1, 2))
+// 还不支持 es6 module
+// VS code extensions are run in a Node environment which does not natively support modules (So no import and export).
+// yo code only uses import when you create a TypeScript extension. For js extensions, yo code instead uses require:
+// https://stackoverflow.com/questions/61298183/syntax-error-cannot-use-import-statement-outside-a-module
+console.log(add(1, 2))
 
 function findIndex(nums) {
   for (let i = nums.length - 2; i>=0; i--) {
